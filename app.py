b121b79367496d9330a8b2556d9e763859a90dce
alysedesouza <166676306+alysedesouza@users.noreply.github.com>
2024-06-03 14:43:46 +0930

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pyproj import Transformer
 app = Flask(__name__)
 
 # Initialize transformers
-transformer_wgs84_to_gda2020 = Transformer.from_crs("EPSG:4326", "EPSG:7850")  # WGS84 to GDA2020
+transformer_wgs84_to_gda2020_zone54 = Transformer.from_crs("EPSG:4326", "EPSG:7855")  # WGS84 to GDA2020 Zone 54
 
 @app.route('/')
 def home():
@@ -16,8 +16,8 @@ def convert_wgs84_to_gda2020():
     latitude = data['latitude']
     longitude = data['longitude']
 
-    # Convert WGS84 to GDA2020
-    converted_easting, converted_northing = transformer_wgs84_to_gda2020.transform(latitude, longitude)
+    # Convert WGS84 to GDA2020 Zone 54
+    converted_easting, converted_northing = transformer_wgs84_to_gda2020_zone54.transform(latitude, longitude)
 
     response = {
         'converted_gda2020': {
