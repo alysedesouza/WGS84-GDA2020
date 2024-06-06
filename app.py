3bbf831e73154b4b06985ec26f49bed2828d8781
alysedesouza <166676306+alysedesouza@users.noreply.github.com>
2024-06-06 09:39:31 +0930

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,34 @@ from pyproj import Transformer
 
 app = Flask(__name__)
 
-# Initialize transformers
-transformer_wgs84_to_gda2020_zone54 = Transformer.from_crs("EPSG:4326", "EPSG:7855")  # WGS84 to GDA2020 Zone 54
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/convert_wgs84_to_gda2020', methods=['POST'])
-def convert_wgs84_to_gda2020():
+@app.route('/convert_coordinates', methods=['POST'])
+def convert_coordinates():
     data = request.json
-    latitude = data['latitude']
-    longitude = data['longitude']
+    zone = int(data['zone'])
+    latitude = float(data['latitude'])
+    longitude = float(data['longitude'])
 
-    # Convert WGS84 to GDA2020 Zone 54
-    converted_easting, converted_northing = transformer_wgs84_to_gda2020_zone54.transform(latitude, longitude)
+    # Define source and destination CRS codes for WGS84 to GDA2020 conversion
+    source_crs = 4326
+    destination_crs = 7800 + zone
 
-    response = {
+    # Perform WGS84 to GDA2020 conversion
+    transformer_wgs84_to_gda2020 = Transformer.from_crs(source_crs, destination_crs)
+    transformed_point_gda2020 = transformer_wgs84_to_gda2020.transform(longitude, latitude)
+
+
+    return jsonify({
         'converted_gda2020': {
-            'easting': converted_easting,
-            'northing': converted_northing
+            'easting': transformed_point_gda2020[0],
+            'northing': transformed_point_gda2020[1]
+        },
+     
         }
-    }
-    return jsonify(response)
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
